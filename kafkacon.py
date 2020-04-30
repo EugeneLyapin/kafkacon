82d233db0ccfb69e8a4834171cbec0f82eadb26f
Cyberjohn00 <lev@nx1>
2020-04-30 21:14:39 +0300

    feature/confluent-kafka-magic-bytes: initial commit;

--- kafkacon.py
+++ kafkacon.py
@@ -9,14 +9,13 @@ basepath = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(str(pathlib.Path(basepath, "modules")))
 import config
 from debug import errx, debug, trace
-from Kafka import Consumer
+from Kafka import KafkaConsumer
 from KMS import KMS
 
 def main():
-    consumer = Consumer(conf=conf)
+    consumer = KafkaConsumer(conf=conf)
     CiphertextBlob = consumer.readMessageByPartitionOffsetAvro()
     trace(CiphertextBlob)
-    errx()
     CiphertextBlob = 'AQICAHhJQblkxQwd25zGDR3lbyJ2t+DNP98Mw8bSKctqHqSS7AGdEYy39rHwqpkcPDzXy2QIAAAAbTBrBgkqhkiG9w0BBwagXjBcAgEAMFcGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMC7BlNXPlD1H00MRtAgEQgCoj1FXHltlKtwOsZ5lSC9IHjd18A2nc66R/G8Gk0p39wEnqwa0U4MmonGY='
     kms = KMS(conf=conf)
     debug(level=1, obj=kms, service=kms.service)
